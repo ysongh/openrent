da159d247d3bfe06e768569e0aa14006ff56a7a3
You Song Hou <yousonghou@yahoo.com>
2019-11-09 15:51:07 -0500

    add state and the logic

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 
 class Home extends Component{
+    state = {
+        address: "",
+        apt: "",
+        room: "",
+        price: ""
+    }
+
+    onChange(e){
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+    onSubmit(){
+        console.log(this.state.address);
+        console.log(this.state.apt);
+        console.log(this.state.room);
+        console.log(this.state.price);
+    }
+
     render(){
         return(
             <div>
@@ -8,28 +26,53 @@ class Home extends Component{
                     <h1>Open Rent</h1>
                     <div className="row">
                         <div className="input-field col s12">
-                        <input type="text" className="validate" placeholder="123 Street Ave"/>
-                        <label for="address">Address</label>
+                        <input
+                            type="text" 
+                            className="validate"
+                            name="address"
+                            placeholder="123 Street Ave"
+                            value={this.state.address}
+                            onChange={this.onChange.bind(this)} />
+                        <label htmlFor="address">Address</label>
                         </div>
                     </div>
                     <div className="row">
                         <div className="input-field col s12">
-                        <input type="number" className="validate" placeholder="1"/>
-                        <label for="apt">Apt Number</label>
+                        <input
+                            type="number"
+                            className="validate"
+                            name="apt"
+                            placeholder="1"
+                            value={this.state.apt}
+                            onChange={this.onChange.bind(this)} />
+                        <label htmlFor="apt">Apt Number</label>
                         </div>
                     </div>
                     <div className="row">
                         <div className="input-field col s12">
-                        <input type="number" className="validate" placeholder="4"/>
-                        <label for="room">Number of Rooms</label>
+                        <input
+                            type="number" 
+                            className="validate"
+                            name="room"
+                            placeholder="4"
+                            value={this.state.room}
+                            onChange={this.onChange.bind(this)} />
+                        <label htmlFor="room">Number of Rooms</label>
                         </div>
                     </div>
                     <div className="row">
                         <div className="input-field col s12">
-                        <input type="number" className="validate" placeholder="1900.00"/>
-                        <label for="price">Price</label>
+                        <input
+                            type="number"
+                            className="validate"
+                            placeholder="1900.00"
+                            name="price"
+                            value={this.state.price}
+                            onChange={this.onChange.bind(this)} />
+                        <label htmlFor="price">Price</label>
                         </div>
                     </div>
+                    <button className="btn waves-effect waves-light" onClick={this.onSubmit.bind(this)} >Submit</button>
                 </div>
             </div>
         )
