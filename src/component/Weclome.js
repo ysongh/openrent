608326caa539f27f9624371cbbe09a7192f7b557
You Song Hou <yousonghou@yahoo.com>
2019-11-10 00:10:11 -0500

    add a background color

--- src/component/Weclome.js
+++ src/component/Weclome.js
@@ -6,24 +6,24 @@ const Weclome = () => {
         <div className="weclome">
             <h1 className="weclome__title mb-2">Weclome to Open Rent</h1>
             <p className="weclome__paragraph">Bringing transparency to New York Rental Market</p>
-            <div className="">
+            <div className="weclome__area">
                 <div className="row">
-                    <div className="col s9">
+                    <div className="col s10">
                         <label for="input_text">Lease Public Key</label>
                         <input id="input_text" type="text" />
                     </div>
-                    <div className="col s3">
+                    <div className="col s2">
                         <Link to="/" className="waves-effect waves-light btn translateY-2">
                              >
                         </Link>
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col s9">
+                    <div className="col s10">
                         <label for="input_text">Building Address</label>
                         <input id="input_text" type="text" />
                     </div>
-                    <div className="col s3">
+                    <div className="col s2">
                         <Link to="/addressList" className="waves-effect waves-light btn translateY-2">
                             >
                         </Link>
